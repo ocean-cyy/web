d3b30fc6fc971c1c0df0183d37b2e87ef150c2c8
许清泉 <xuqingquan1995@gmail.com>
2021-10-14 19:50:11 +0800

    去除adblock

--- web/src/main/java/top/xuqingquan/web/nokernel/init.kt
+++ web/src/main/java/top/xuqingquan/web/nokernel/init.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import com.tencent.smtt.export.external.TbsCoreSettings
 import com.tencent.smtt.sdk.QbSdk
 import com.tencent.smtt.sdk.TbsListener
-import org.adblockplus.libadblockplus.android.AdblockEngineProvider
-import org.adblockplus.libadblockplus.android.settings.AdblockHelper
 import top.xuqingquan.utils.Timber
-import top.xuqingquan.web.R
 
 /**
  * Created by 许清泉 on 2020/7/7 22:24
@@ -56,43 +53,4 @@ fun initTbs(
     }
     //x5内核初始化接口
     QbSdk.initX5Environment(context, callback)
-}
-
-/**
- * 广告引擎初始化
- */
-fun initAdblock(
-    context: Context,
-    urlToResourceIdMap: Map<String, Int>? = null,
-    engineCreatedListener: AdblockEngineProvider.EngineCreatedListener? = null,
-    engineDisposedListener: AdblockEngineProvider.EngineDisposedListener? = null
-) {
-    if (AdblockHelper.get().isInit) {
-        return
-    }
-    val map = urlToResourceIdMap
-        ?: mapOf(
-            "https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt" to R.raw.easylistchina_easylist,//中文规则
-            "https://easylist-downloads.adblockplus.org/exceptionrules.txt" to R.raw.exceptionrules,//可接受广告
-            "https://cdn.adblockcdn.com/filters/adblock_custom.txt" to R.raw.adblock_custom,//自定义规则
-            "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt" to R.raw.abp_filters_anti_cv//反规避拦截规则
-        )
-    val provider =
-        AdblockHelper.get().init(context, context.cacheDir.absolutePath, "adblock")
-    provider.preloadSubscriptions("rules", map)
-    if (engineCreatedListener != null) {
-        provider.addEngineCreatedListener(engineCreatedListener)
-    } else {
-        provider.addEngineCreatedListener {
-            Timber.d("addEngineCreatedListener--${it.isEnabled}")
-        }
-    }
-    if (engineDisposedListener != null) {
-        provider.addEngineDisposedListener(engineDisposedListener)
-    } else {
-        provider.addEngineDisposedListener {
-            Timber.d("addEngineDisposedListener")
-        }
-    }
-    provider.provider.retain(true)
 }
